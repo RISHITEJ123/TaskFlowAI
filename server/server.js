7c71641c46017c683737936fb5c56ae4d0230a70
RISHITEJ123 <rishitej.aggarapu@gmail.com>
2025-04-15 22:19:22 +0530

    Fix missing Task model and task APIs

--- server/server.js
+++ server/server.js
@@ -5,13 +5,32 @@ require('dotenv').config();
 const app = express();
 const port = 3000;
 
-// Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+app.use(express.json());
+
+mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log('Connected to MongoDB'))
   .catch((err) => console.error('MongoDB connection error:', err));
+
+const Task = require('../models/Task');
+
+app.post('/tasks', async (req, res) => {
+  try {
+    const task = new Task(req.body);
+    await task.save();
+    res.status(201).send(task);
+  } catch (err) {
+    res.status(400).send({ error: err.message });
+  }
+});
+
+app.get('/tasks', async (req, res) => {
+  try {
+    const tasks = await Task.find();
+    res.send(tasks);
+  } catch (err) {
+    res.status(500).send({ error: err.message });
+  }
+});
 
 app.get('/', (req, res) => {
   res.send('TaskFlowAI Server Running!');
