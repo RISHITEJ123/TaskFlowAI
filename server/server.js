24310526f0baea38abff47d3e13eb2f0a0f5f85d
RISHITEJ123 <rishitej.aggarapu@gmail.com>
2025-04-15 21:59:35 +0530

    Connect server to MongoDB with mongoose

--- server/server.js
+++ server/server.js
@@ -1,6 +1,17 @@
 const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
 const app = express();
 const port = 3000;
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((err) => console.error('MongoDB connection error:', err));
 
 app.get('/', (req, res) => {
   res.send('TaskFlowAI Server Running!');
